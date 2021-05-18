dae856d10969a99fd5c6e2951e906df3fb8121e5
Jvala2 <joevalancy@gmail.com>
2021-05-18 13:00:52 -0400

    Updated files to clean up notes

--- data/ticketData.js
+++ data/ticketData.js
@@ -2,12 +2,9 @@
 
 const tableArray = [
   {
-    patientID: '',
-    patientName: '',
-    patientDob: '',
 
   },
 ];
 
-// Note how we export the array. This makes it accessible to other files using require.
+
 module.exports = tableArray;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,8 @@
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
 
 const path = require('path');
 
-// ROUTING
 
 module.exports = (app) => {
-  // => HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
 
   app.get('/tickets', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/tickets.html'));
@@ -18,7 +12,6 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, '../public/patient.html'));
   });
 
-  // If no matching route is found default to home
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/home.html'));
   });
