25ce9f5f2f42403f7d43b2f7130b8f2893f40ec8
Jvala2 <joevalancy@gmail.com>
2021-05-18 18:53:29 -0400

    Error on line 186

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,13 +21,7 @@ module.exports = (app) => {
       res.json(true);
   });
 
-  app.get('/api/patient', (req, res) => {
-    const search = req.query.search.toLowerCase();
-    if (search) {
-      const tickets = ticketData.filter(ticket => ticket.patientID.toLowerCase().includes(search)||ticket.patientName.toLowerCase().includes(search)||ticket.patientDob.toLowerCase().includes(search))
-      res.json(tickets)
-    } else {res.json(ticketData)}
-    });
+
 
 
   app.post('/api/clear', (req, res) => {
